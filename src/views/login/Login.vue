bd17f0c1c720dbeb7c81cad6bcf73948f46921f3
npcxh <bakanpcxh@gmail.com>
2021-03-13 21:55:55 +0800

    登录请求封装

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -21,9 +21,9 @@
 
 <script>
 import { useRouter } from 'vue-router'
-import axios from 'axios'
+import { post } from '../../utils/request.js'
 import { reactive } from 'vue'
-axios.defaults.headers.post['Content-Type'] = 'application/json'
+
 export default {
   name: 'Login',
   setup () {
@@ -32,17 +32,21 @@ export default {
       password: ''
     })
     const router = useRouter();
-    const handleLogin = () => {
-      axios.post('https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd/api/user/login', {
-        username: data.username,
-        password: data.password
-      }).then(() => {
-        alert('成功');
-        // localStorage.isLogin = true;
-        // router.push({ name: 'Home' });
-      }).catch(() => {
-        alert('登录失败');
-      });
+    const handleLogin = async () => {
+      try {
+        const result = await post('/api/user/login', {
+          username: data.username,
+          password: data.password
+        });
+        if (result?.errno === 0) {
+          localStorage.isLogin = true;
+          router.push({ name: 'Home' });
+        } else {
+          alert('失败');
+        }
+      } catch (e) {
+        alert('请求失败');
+      }
     }
     const handleRegisterClick = () => {
       router.push({ name: 'Register' });
