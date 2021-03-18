fb1fb93478861f3220fb726b234ca22a01bc346e
npcxh <bakanpcxh@gmail.com>
2021-03-18 16:27:26 +0800

    店铺顶部搜索框+首页与店铺跳转

--- src/views/home/Nearby.vue
+++ src/views/home/Nearby.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="nearby">
       <h3 class="nearby__title">附近店铺</h3>
-      <ShopInfo
-       v-for="item in nearbyList" :key="item._id"
-       :item="item"/>
+      <router-link
+       to="/shop"
+       v-for="item in nearbyList" :key="item._id">
+        <ShopInfo :item="item"/>
+      </router-link>
     </div>
 </template>
 
@@ -49,6 +51,9 @@ export default {
     font-size: .18rem;
     font-weight: normal;
     color: $content-fontcolor;
+  }
+  a {
+    text-decoration: none;
   }
   &__item{
     display: flex;
--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="warpper">
-      <ShopInfo :item="item" :hideBorder="true"/>
+    <div class="search">
+      <div class="search__back iconfont"
+       @click="handleBackClick"
+      >&#xe60d;</div>
+      <div class="search__content">
+        <span class="search__content__icon iconfont">&#xe628;</span>
+        <input class="search__content__input" placeholder="请输入商品名称">
+      </div>
+    </div>
+    <ShopInfo :item="item" :hideBorder="true"/>
   </div>
 </template>
 
 <script>
 import ShopInfo from '../../components/ShopInfo'
+import { useRouter } from 'vue-router'
 export default {
   name: 'Shop',
   components: { ShopInfo },
   setup () {
+    const router = useRouter();
     const item = {
       _id: '1',
       name: '沃尔玛',
@@ -19,7 +30,10 @@ export default {
       expressPrice: 5,
       sloganL: 'VIP尊享满89元减4元运费券'
     }
-    return { item }
+    const handleBackClick = () => {
+      router.back();
+    }
+    return { item, handleBackClick }
   }
 }
 </script>
@@ -27,5 +41,40 @@ export default {
 <style lang="scss" scoped>
   .warpper {
     padding: 0 .18rem;
+  }
+
+  .search {
+    display: flex;
+    margin: .2rem 0 .16rem 0;
+    line-height: .32rem;
+    &__back {
+      width: .3rem;
+      font-size: .24rem;
+      color: #B6B6B6;
+    }
+    &__content {
+      display: flex;
+      flex: 1;
+      background: #f5f5f5;
+      border-radius: .16rem;
+      &__icon {
+        width: .44rem;
+        text-align: center;
+        color: #B7B7B7;
+      }
+      &__input {
+        width: 100%;
+        padding-right: .2rem;
+        border: none;
+        outline: none;
+        background: none;
+        height: .32rem;
+        font-size: .14rem;
+        color: #333;
+        &::placeholder{
+          color: #333;
+        }
+      }
+    }
   }
 </style>
